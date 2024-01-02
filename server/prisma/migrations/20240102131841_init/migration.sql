ce2ba26cc0207bbd24333c102801248ccacb957c
Maryem-Harmessi <hermassimyriam@gmail.com>
2024-01-02 14:19:10 +0100

    modify schema

--- server/prisma/migrations/20240102131841_init/migration.sql
+++ server/prisma/migrations/20240102131841_init/migration.sql
@@ -9,7 +9,6 @@ CREATE TABLE "User" (
     "phone_number" TEXT NOT NULL,
     "age" INTEGER NOT NULL,
     "mood" INTEGER[],
-    "sleep" INTEGER[],
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
@@ -48,13 +47,25 @@ CREATE TABLE "Journal" (
 );
 
 -- CreateTable
-CREATE TABLE "Appointement" (
+CREATE TABLE "Appointment" (
     "id" SERIAL NOT NULL,
     "patientId" INTEGER NOT NULL,
     "doctorId" INTEGER NOT NULL,
-    "schedule" TIMESTAMP(3) NOT NULL,
+    "appDetails" TEXT NOT NULL,
+    "appStatus" TEXT NOT NULL DEFAULT 'available',
+    "appReview" INTEGER NOT NULL,
+    "appFeedback" TEXT NOT NULL,
+    "slotId" INTEGER NOT NULL,
 
-    CONSTRAINT "Appointement_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Appointment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Slot" (
+    "id" SERIAL NOT NULL,
+    "windowId" INTEGER NOT NULL,
+
+    CONSTRAINT "Slot_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -76,7 +87,7 @@ CREATE TABLE "Comment" (
     "downvote" INTEGER NOT NULL,
     "postId" INTEGER NOT NULL,
     "userId" INTEGER NOT NULL,
-    "tagId" INTEGER NOT NULL,
+    "tagId" INTEGER,
 
     CONSTRAINT "Comment_pkey" PRIMARY KEY ("id")
 );
@@ -128,6 +139,41 @@ CREATE TABLE "MedSchedule" (
 );
 
 -- CreateTable
+CREATE TABLE "Habit" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Habit_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "TrackHabit" (
+    "id" SERIAL NOT NULL,
+    "habitId" INTEGER NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "tracker" INTEGER[],
+
+    CONSTRAINT "TrackHabit_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "window" (
+    "id" SERIAL NOT NULL,
+    "doctorId" INTEGER NOT NULL,
+    "startingTime" TIMESTAMP(3) NOT NULL,
+    "endingTime" TIMESTAMP(3) NOT NULL,
+    "duration" INTEGER NOT NULL,
+
+    CONSTRAINT "window_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_waitlist" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateTable
 CREATE TABLE "_FlairToForumPost" (
     "A" INTEGER NOT NULL,
     "B" INTEGER NOT NULL
@@ -152,7 +198,19 @@ CREATE UNIQUE INDEX "Doctor_email_key" ON "Doctor"("email");
 CREATE UNIQUE INDEX "Meds_name_key" ON "Meds"("name");
 
 -- CreateIndex
+CREATE UNIQUE INDEX "Appointment_slotId_key" ON "Appointment"("slotId");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "Flair_name_key" ON "Flair"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Habit_name_key" ON "Habit"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_waitlist_AB_unique" ON "_waitlist"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_waitlist_B_index" ON "_waitlist"("B");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "_FlairToForumPost_AB_unique" ON "_FlairToForumPost"("A", "B");
@@ -170,10 +228,16 @@ CREATE INDEX "_GroupToUser_B_index" ON "_GroupToUser"("B");
 ALTER TABLE "Journal" ADD CONSTRAINT "Journal_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Appointement" ADD CONSTRAINT "Appointement_patientId_fkey" FOREIGN KEY ("patientId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Appointment" ADD CONSTRAINT "Appointment_patientId_fkey" FOREIGN KEY ("patientId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Appointement" ADD CONSTRAINT "Appointement_doctorId_fkey" FOREIGN KEY ("doctorId") REFERENCES "Doctor"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Appointment" ADD CONSTRAINT "Appointment_doctorId_fkey" FOREIGN KEY ("doctorId") REFERENCES "Doctor"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Appointment" ADD CONSTRAINT "Appointment_slotId_fkey" FOREIGN KEY ("slotId") REFERENCES "Slot"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Slot" ADD CONSTRAINT "Slot_windowId_fkey" FOREIGN KEY ("windowId") REFERENCES "window"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "ForumPost" ADD CONSTRAINT "ForumPost_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -185,7 +249,7 @@ ALTER TABLE "Comment" ADD CONSTRAINT "Comment_postId_fkey" FOREIGN KEY ("postId"
 ALTER TABLE "Comment" ADD CONSTRAINT "Comment_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Comment" ADD CONSTRAINT "Comment_tagId_fkey" FOREIGN KEY ("tagId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_tagId_fkey" FOREIGN KEY ("tagId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Article" ADD CONSTRAINT "Article_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "Doctor"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -204,6 +268,21 @@ ALTER TABLE "MedSchedule" ADD CONSTRAINT "MedSchedule_medId_fkey" FOREIGN KEY ("
 
 -- AddForeignKey
 ALTER TABLE "MedSchedule" ADD CONSTRAINT "MedSchedule_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TrackHabit" ADD CONSTRAINT "TrackHabit_habitId_fkey" FOREIGN KEY ("habitId") REFERENCES "Habit"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TrackHabit" ADD CONSTRAINT "TrackHabit_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "window" ADD CONSTRAINT "window_doctorId_fkey" FOREIGN KEY ("doctorId") REFERENCES "Doctor"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_waitlist" ADD CONSTRAINT "_waitlist_A_fkey" FOREIGN KEY ("A") REFERENCES "Slot"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_waitlist" ADD CONSTRAINT "_waitlist_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "_FlairToForumPost" ADD CONSTRAINT "_FlairToForumPost_A_fkey" FOREIGN KEY ("A") REFERENCES "Flair"("id") ON DELETE CASCADE ON UPDATE CASCADE;
