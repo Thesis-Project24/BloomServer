faaed55e5c21aef43ba4dd586cada5213fa5f28f
Maryem-Harmessi <hermassimyriam@gmail.com>
2024-01-04 10:42:01 +0100

    slots

--- server/modules/appointments/slots/slotsRoute.ts
+++ server/modules/appointments/slots/slotsRoute.ts
@@ -1,6 +1,8 @@
 import express from "express"
-import {addSlots} from './slotsController'
+import {addSlots,getSlots,getSlot} from './slotsController'
 const router = express.Router();
 
 router.post('/:windowId',addSlots)
+router.get('/slot/:slotId',getSlot)
+router.get('/:windowId',getSlots)
 export default router;
