39d1cb081723c324fe2b3da689905122223c20ca
Hyba <hibazoglami2018@gmail.com>
2024-01-05 01:22:35 +0100

    back doctors done

--- server/modules/doctors/doctorController.ts
+++ server/modules/doctors/doctorController.ts
@@ -6,6 +6,38 @@ const prisma=new PrismaClient();
 
 
 
+export const getDoctors = async (req:Request,res:Response)=>{
+    try {
+        const doc = await prisma.doctor.findMany()
+        return res.json(doc)
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error);
+    }
+    }
+
+    export const getOneDoc = async (req:Request,res:Response)=>{
+        const productId = JSON.parse(req.params.id);
+        try {
+            const doc = await prisma.doctor.findMany({
+                where: {
+                  id: productId,
+                },
+              })
+            return res.json(doc)
+        } catch (error) {
+            console.log(error);
+            res.status(500).send(error);
+        }
+        }
+
+
+
+
+
+
+
+
 export const addDoctor = async (req:Request,res:Response)=>{
     const {email,first_name,last_name,phone_number,specialty,profile_picture,address}:Doctor = req.body
     try {
