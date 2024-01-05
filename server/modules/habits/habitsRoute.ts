203c9ce9f4ca227021950e644ac764ddfa8abcf4
aminezouari12 <aminezouari262@gmail.com>
2024-01-05 17:21:21 +0100

    done

--- server/modules/habits/habitsRoute.ts
+++ server/modules/habits/habitsRoute.ts
@@ -1,9 +1,11 @@
 import express from 'express'
-import { assignHabit, creatHabit, getAllHabitsFromUser, postSatisfaction } from './habitsController';
+import { assignHabit, creatHabit, deleteHabit, getAllHabits, getAllHabitsFromUser, postSatisfaction } from './habitsController';
 const router = express.Router();
 
 router.post("/createhabit",creatHabit)
 router.post("/assignHabit",assignHabit)
 router.get("/getHabits/:id",getAllHabitsFromUser)
+router.get("/getAllHabits",getAllHabits)
 router.put("/satisHabit",postSatisfaction)
+router.delete("/deleteAssignedHabit",deleteHabit)
 export default router;
