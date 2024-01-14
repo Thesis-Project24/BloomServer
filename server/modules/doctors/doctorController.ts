cc5c7c4ab2f1aad43ab2e1dbd904b0b601b78453
raja Mansour <rajamansour91@gmail.com>
2024-01-14 23:03:24 +0100

    fixing errors

--- server/modules/doctors/doctorController.ts
+++ server/modules/doctors/doctorController.ts
@@ -16,12 +16,14 @@ export const getDoctors = async (req: Request, res: Response) => {
 
 export const getOneDoc = async (req: Request, res: Response) => {
   const productId = JSON.parse(req.params.id);
+  console.log(productId)
   try {
     const doc = await prisma.doctor.findUnique({
       where: {
         id: productId,
       },
     });
+    console.log(doc,"eeefwewwrfkhfwhfwhfwleflwflewf");
     return res.json(doc);
   } catch (error) {
     console.log(error);
@@ -55,7 +57,6 @@ export const addDoctor = async (req: Request, res: Response) => {
       data: docBody,
     });
 
-<<<<<<< HEAD
     return res.json(doc);
   } catch (error) {
     console.log(error);
@@ -75,23 +76,6 @@ export const upDateDoc = async (req: Request, res: Response) => {
     address,
     bio,
   }: Doctor = req.body;
-=======
-    export const getOneDoc = async (req:Request,res:Response)=>{
-        const id = JSON.parse(req.params.id);;
-        try {
-
-            const doc = await prisma.doctor.findUnique({
-                where: {
-                  id: id,
-                },
-              })
-            return res.json(doc)
-        } catch (error) {
-            console.log(error);
-            res.status(500).send(error);
-        }
-        }
->>>>>>> 7e05dae9b5dad3762453f2769a1e458380535b09
 
   try {
     const docBody: Doctor = {
@@ -116,7 +100,6 @@ export const upDateDoc = async (req: Request, res: Response) => {
   }
 };
 
-<<<<<<< HEAD
 export const addReviewDoc = async (req: Request, res: Response) => {
   try {
     const Appoint = await prisma.appointment.findMany({
@@ -143,128 +126,33 @@ export const addReviewDoc = async (req: Request, res: Response) => {
     res.send(doctor);
   } catch (error) {}
 };
-=======
 
+export const getDocSpecialist = async (req: Request, res: Response) => {
+  const { specialty } = req.params;
+  try {
+    const doc = await prisma.doctor.findMany({
+      where: {
+        specialty: specialty,
+      },
+    });
+    return res.json(doc);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+};
 
-
-
-
-export const addDoctor = async (req:Request,res:Response)=>{
-    const {email,first_name,last_name,phone_number,specialty,profile_picture,address , bio}:Doctor = req.body
-    try {
-       
-        const docBody : Doctor = {
-            email,
-            first_name,
-            last_name,
-            phone_number,
-            specialty,
-            profile_picture,
-            address,
-            bio
-        }
-        const doc = await prisma.doctor.create({
-            data: docBody
-        })
-        
-        return res.json(doc)
-    } catch (error) {
-        console.log(error);
-        res.status(500).send(error)
-        
-        
-    }
-    }
-
-    
-export const upDateDoc = async (req:Request,res:Response)=>{
-    const {id ,email,first_name,last_name,phone_number,specialty,profile_picture,address , bio}:Doctor = req.body
-   
-    try {
-       
-        const docBody : Doctor = {
-            email,
-            first_name,
-            last_name,
-            phone_number,
-            specialty,
-            profile_picture,
-            address,
-            bio
-        }
-        const doc = await prisma.doctor.update({
-            where: { id },
-            data: docBody
-        })
-        
-        return res.json(doc)
-    } catch (error) {
-        console.log(error);
-        res.status(500).send(error)
-        
-        
-    }
-    }
-
-    export const addReviewDoc= async(req:Request,res:Response)=>{
-        try {
-            const Appoint =await  prisma.appointment.findMany({
-                where:{
-                    doctorId:Number(req.params.docotrId),
-                    appReview: {
-                        gte:0
-                    }
-                }
-            })
-            const doctorReview= Appoint.reduce((review:number,app)=>{
-                return review=review + app.appReview
-        
-            },0)
-            const doctor = prisma.doctor.update({
-                where:{
-                    id:Number(req.params.docotrId)
-                },
-                data:{
-                    review:{
-                        set:doctorReview/ Appoint.length
-                    }
-                }
-            })
-            res.send(doctor)
-        }
-        catch(error){
-        }
-    }
- 
-    export const getDocSpecialist = async (req:Request,res:Response)=>{
-        const {specialty} = req.params
-        try {
-
-            const doc = await prisma.doctor.findMany({
-                where: {
-                 specialty: specialty,
-                },
-              })
-            return res.json(doc)
-        } catch (error) {
-            console.log(error);
-            res.status(500).send(error);
-        }
-        }
-
-        export const getDocGender = async (req:Request,res:Response)=>{
-            const {gender} = req.params
-            try {
-    
-                const doc = await prisma.doctor.findMany({
-                    where: {
-                        gender: gender,
-                    },
-                  })
-                return res.json(doc)
-            } catch (error) {
-                console.log(error);
-                res.status(500).send(error);
-            }
-            }
->>>>>>> 7e05dae9b5dad3762453f2769a1e458380535b09
+export const getDocGender = async (req: Request, res: Response) => {
+  const { gender } = req.params;
+  try {
+    const doc = await prisma.doctor.findMany({
+      where: {
+        gender: gender,
+      },
+    });
+    return res.json(doc);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+}
