136df6652c71a710fb28528a27dd53ff295e83a1
raja Mansour <rajamansour91@gmail.com>
2024-01-15 10:35:45 +0100

    bloom

--- server/modules/users/userRoute.ts
+++ server/modules/users/userRoute.ts
@@ -1,8 +1,9 @@
 import express from 'express'
-import {signUp,signIn,deleteAccount,updateInfo} from '../../modules/users/userController'
+import {signUp,signIn,deleteAccount,updateInfo,getOne} from '../../modules/users/userController'
 const router = express.Router();
 router.post("/signup",signUp)
 router.post("/signin",signIn)
 router.put("/:userId",updateInfo)
 router.delete("/:id",deleteAccount)
+router.get("/:userId",getOne)
 export default router;
