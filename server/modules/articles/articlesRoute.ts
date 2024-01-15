9466f040a7a84bceb9282823553226fe69f76b2d
khadhKadh <azizkh103@gmail.com>
2024-01-15 15:27:35 +0100

    articles back done

--- server/modules/articles/articlesRoute.ts
+++ server/modules/articles/articlesRoute.ts
@@ -1,4 +1,11 @@
 import express from "express"
+import { createArticle, getAllArticles, getSavedArticle, saveArticle, searchArticles } from "./articlesController";
 const router = express.Router();
+
+router.post("/saveArticle",saveArticle)
+router.get("/getArticles",getAllArticles)
+router.post("/postArticle",createArticle)
+router.get("/savedArticles",getSavedArticle)
+router.get("/searchArticle",searchArticles)
 
 export default router;
